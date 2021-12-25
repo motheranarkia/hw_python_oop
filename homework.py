738d2ec8c978c6581339f7cfd6bc961a91342d38
motheranarkia <96246181+motheranarkia@users.noreply.github.com>
2021-12-25 23:10:03 +0300

    Правки n

--- homework.py
+++ homework.py
@@ -81,9 +81,9 @@ class SportsWalking(Training):
     def get_spent_calories(self) -> float:
         return (
             (self.coeff_calorie_3 * self.weight
-            + (self.get_mean_speed()**2 // self.height)
-            * self.coeff_calorie_4 * self.weight)
-            * (self.duration * SportsWalking.in_minutes)
+          + (self.get_mean_speed()**2 // self.height)
+           * self.coeff_calorie_4 * self.weight)
+          * (self.duration * SportsWalking.in_minutes)
         )
 
 
